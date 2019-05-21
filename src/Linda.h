e8787948ebdb0499ed224e1c6986d9ec01ea5e78
Kamil Mankowski (kam193) <kam193@wp.pl>
2019-05-21 19:05:53 +0200

    Store size of tuple, limits max tuple size, split tuple for elements and describes

--- src/Linda.h
+++ src/Linda.h
@@ -4,9 +4,14 @@
 #define SRC_LINDA_H_
 
 #include <cstdint>
+#include <cstdlib>
+
+namespace uxp {
 
 class Linda {
  public:
+  static const size_t MAX_TUPLE_SIZE = 100;
+
   enum Type : int16_t { INT, FLOAT, STRING };
 
   struct Element {
@@ -36,16 +41,23 @@ class Linda {
   };
 
   struct Tuple {
-    alignas(int32_t) char elements[];  // We expected an array of pointers, yes?
-    // Should we have size given here or as a first element od array?
+    size_t size;
+    struct Element* elements[MAX_TUPLE_SIZE];
+  };
+
+  struct TupleDesc {
+    size_t size;
+    struct ElementDesc* elements[MAX_TUPLE_SIZE];
   };
 
   Linda();
   explicit Linda(int memory_identifier);
 
-  void outpute(Tuple tuple);
-  Tuple input(Tuple describe, unsigned int timeout_ms);
-  Tuple read(Tuple describe, unsigned int timeout_ms);
+  void output(Tuple tuple);
+  Tuple input(TupleDesc describe, unsigned int timeout_ms);
+  Tuple read(TupleDesc describe, unsigned int timeout_ms);
 };
+
+}  // namespace uxp
 
 #endif  // SRC_LINDA_H_
