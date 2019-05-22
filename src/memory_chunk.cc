fb6b0ed8cd19d92bddf3e5534008bee9d6547218
LubiePierogi <26669430+LubiePierogi@users.noreply.github.com>
2019-05-22 18:22:43 +0200

    Class MemotyChunk and CMake files.

---
+++ src/memory_chunk.cc
@@ -0,0 +1,182 @@
+// Copyright 2019 UXP1A Students Team
+
+#include "src/memory_chunk.h"
+
+#include <cassert>
+#include <stdexcept>
+#include <iostream>
+
+namespace uxp {
+/*
+void *MemoryChunk::CreateNewMem_(const char *path, int key_id) {
+  int shm_key = ftok(path, key_id);
+  if (shm_key < 0)
+    return nullptr;
+  int shm_id = shmget(shm_key, MEMORY_SIZE, IPC_CREAT | IPC_EXCL | 0660);
+  if (shm_id < 0) {
+    return nullptr;
+  }
+  void *mem = shmat(shm_id, nullptr, 0);
+  if (mem == reinterpret_cast<void *>(-1)) {
+    shmctl(shm_id, IPC_RMID, nullptr);
+    return nullptr;
+  }
+  return mem;
+}
+*/
+
+
+MemoryChunk::MemoryChunk()
+  : state_(State::BLANK), shm_id_(0), sem_id_(0), key_(0), address_(nullptr),
+    size_(0) {}
+
+MemoryChunk::MemoryChunk(const char *path, size_t size)
+  : MemoryChunk() {
+  int e = CreateNewMem_(path, size);
+  if (e < 0)
+    throw std::runtime_error("");
+}
+
+MemoryChunk::MemoryChunk(const char *path)
+  : MemoryChunk() {
+  int e = AttachNotNew_(path);
+  if (e < 0)
+    throw std::runtime_error("");
+}
+
+MemoryChunk::~MemoryChunk() {
+  if (IsOpen()) {
+    Close_();
+  }
+}
+
+int MemoryChunk::CreateNewMem_(const char *path, size_t size) {
+  assert(state_ == BLANK);
+
+  int shm_key = ftok(path, PROJ_ID);
+  int sem_key = ftok(path, SEM_PROJ_ID);
+  if (shm_key < 0 || sem_key < 0)
+    return -1;
+  int shm_id = shmget(shm_key, size, IPC_CREAT | IPC_EXCL | 0660);
+  if (shm_id < 0) {
+    return -2;
+  }
+  int sem_id = semget(sem_key, 1, IPC_CREAT | IPC_EXCL | 0660);
+  if (sem_id < 0) {
+    return -3;
+  }
+  void *mem = shmat(shm_id, nullptr, 0);
+  if (mem == reinterpret_cast<void *>(-1)) {
+    shmctl(shm_id, IPC_RMID, nullptr);
+    return -4;
+  }
+  // struct sembuf smbf;
+  // smbf.sem_flg = SEM_UNDO | IPC_NOWAIT;
+  // smbf.sem_num = 0;
+  // smbf.sem_op = 1;
+  int semctl_ret = semctl(sem_id, 0, SETVAL, 0);
+  // int semop_ret = semop(sem_id, &smbf, 1);
+  if (semctl_ret < 0 /* || semctl_ret < 0 */) {
+    semctl(sem_id, 0, IPC_RMID);
+    shmctl(shm_id, IPC_RMID, nullptr);
+    return -5;
+    // [TODO] Do clean up correctly.
+  }
+  state_ = CREATED;
+  shm_id_ = shm_id;
+  sem_id_ = sem_id;
+  address_ = mem;
+  key_ = shm_key;
+  struct shmid_ds shmds;
+  int e = shmctl(shm_id, IPC_STAT, &shmds);
+  assert(e >= 0);
+  size_ = shmds.shm_segsz;
+  return 0;
+}
+
+
+int MemoryChunk::AttachNotNew_(const char *path) {
+  assert(state_ == BLANK);
+
+  int shm_key = ftok(path, PROJ_ID);
+  int sem_key = ftok(path, SEM_PROJ_ID);
+  if (shm_key < 0)
+    return -1;
+  int shm_id = shmget(shm_key, 0, 0);
+  if (shm_id < 0) {
+    return -2;
+  }
+  int sem_id = semget(sem_key, 0, 0);
+  if (sem_id < 0) {
+    return -3;
+  }
+  void *mem = shmat(shm_id, nullptr, 0);
+  if (mem == reinterpret_cast<void *>(-1)) {
+    return -3;
+  }
+  struct sembuf smbf;
+  smbf.sem_flg = SEM_UNDO | IPC_NOWAIT;
+  smbf.sem_num = 0;
+  smbf.sem_op = 1;
+  int semop_ret = semop(sem_id, &smbf, 1);
+  if (semop_ret < 0) {
+    return -5;
+  }
+  state_ = GIVEN;
+  shm_id_ = shm_id;
+  sem_id_ = sem_id;
+  address_ = mem;
+  key_ = shm_key;
+  struct shmid_ds shmds;
+  int e = shmctl(shm_id, IPC_STAT, &shmds);
+  assert(e >= 0);
+  size_ = shmds.shm_segsz;
+  return 0;
+}
+
+
+
+int MemoryChunk::AttachNew(const char *path, size_t size) {
+  int e = CreateNewMem_(path, size);
+  if (e < 0)
+    return e;
+  else
+    return shm_id_;
+}
+
+int MemoryChunk::Attach(const char *path) {
+  int e = AttachNotNew_(path);
+  if (e < 0)
+    return e;
+  else
+    return shm_id_;
+}
+
+void MemoryChunk::Close_() {
+  struct sembuf smbf;
+  smbf.sem_op = -1;
+  smbf.sem_num = 0;
+  smbf.sem_flg = IPC_NOWAIT | SEM_UNDO;
+  shmdt(address_);
+  int semop_ret = semop(sem_id_, &smbf, 1);
+  if (semop_ret < 0) {
+    if (errno == EAGAIN) {
+      // Jest dobrze, jeśli tu jesteśmy, to znaczy, że jako ostatni zamykamy
+      // naszą pamięć.
+
+      semctl(sem_id_, 0, IPC_RMID);
+      shmctl(shm_id_, IPC_RMID, nullptr);
+    } else {
+      std::cerr << "blad xd\n";
+    }
+  }
+}
+
+void MemoryChunk::Detach() {
+  if (!IsOpen()) {
+    return;  // Now we do nothing.
+  }
+  return Close_();
+}
+
+}  // namespace uxp
---
+++ src/memory_chunk.h
@@ -0,0 +1,60 @@
+// Copyright 2019 UXP1A Students Team
+
+#ifndef SRC_MEMORY_CHUNK_H_
+#define SRC_MEMORY_CHUNK_H_
+
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+
+namespace uxp {
+class MemoryChunk {
+ public:
+  enum State {
+    BLANK = 0,
+    CREATED = 1,
+    GIVEN = 2,
+  };
+  // static constexpr struct CREATE_t {} CREATE {};
+
+  static constexpr size_t MEMORY_SIZE = 64 * 1024;
+  static constexpr int PROJ_ID = 112;
+  static constexpr int SEM_PROJ_ID = 113;
+
+  MemoryChunk();
+  explicit MemoryChunk(const char *path);
+  explicit MemoryChunk(const char *path, size_t size);
+
+  MemoryChunk(const MemoryChunk&);
+  MemoryChunk &operator=(const MemoryChunk&);
+
+  MemoryChunk(MemoryChunk &&);
+  MemoryChunk &operator=(MemoryChunk &&);
+
+  ~MemoryChunk();
+
+  bool IsOpen() {return state_ != BLANK;}
+  void *GetMem() {return IsOpen() ? address_ : nullptr;}
+  // int GetMemId() {return IsOpen() ? shm_id_ : 0;}
+  size_t GetSize() {return IsOpen() ? size_ : 0;}
+  int Attach(const char *path);
+  int AttachNew(const char *path, size_t size);
+  void Detach();
+
+
+
+ private:
+  int CreateNewMem_(const char *path, size_t size);
+  int AttachNotNew_(const char *path);
+  void Close_();
+
+  State state_;
+  int shm_id_;
+  int sem_id_;
+  key_t key_;
+  void *address_;
+  size_t size_;
+};
+}  // namespace uxp
+
+#endif  // SRC_MEMORY_CHUNK_H_
