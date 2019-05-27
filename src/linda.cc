6405d02d8c7ad7dc2a4869de81fbe374395dae5c
Adam Kapica <37515502+zetraxus@users.noreply.github.com>
2019-05-27 12:59:16 +0200

    Synchronised output input and read functions by using semaphores. (#13)

--- src/linda.cc
+++ src/linda.cc
@@ -1,6 +1,7 @@
 // Copyright 2019 UXP1A Students Team
 
 #include <cassert>
+#include <zconf.h>
 #include "src/linda.h"
 
 namespace uxp {
@@ -9,8 +10,10 @@ void Linda::Output(Tuple tuple) {
   // [TODO] empty tuple
 
   // Only for debug;
+  serviceQueue.p();
+  resourceAccess.p();
+  serviceQueue.v();
   Tuple *tuples = &TupleAt_(0);
-  (void)tuples;
 
   size_t i;
   for (i = 0; i < TUPLES_NUMBER; ++i) {
@@ -19,31 +22,66 @@ void Linda::Output(Tuple tuple) {
       continue;
     }
     *t = tuple;
+    resourceAccess.v();
     return;
   }
+  resourceAccess.v();
   // We did not find empty place. We should wait??
   assert(((void) &Linda::Output, 0));  // Temporary solution.
 }
 
 Tuple Linda::Input(TupleDesc describe, unsigned int timeout_ms) {
   Tuple *t;
-  while ((t = Find_(describe)) == nullptr) {
-    // czekanie??
-  }
-  Tuple ret;
-  ret = *t;
-  ZeroTuple_(t);
-  return ret;
+
+  do{
+    serviceQueue.p();
+    resourceAccess.p();
+    serviceQueue.v();
+
+    t = Find_(describe);
+
+    if(t != nullptr){
+      Tuple ret = *t;
+      ZeroTuple_(t);
+      resourceAccess.v();
+      return ret;
+    }
+    resourceAccess.v();
+  } while (sleep(1));  // or other option for waiting/sleeping
 }
 
 void Linda::ZeroTuple_(Tuple *t) { t->size = 0; }
 
 Tuple Linda::Read(TupleDesc describe, unsigned int timeout_ms) {
   Tuple *t;
-  while ((t = Find_(describe)) == nullptr) {
-    // czekanie??
-  }
-  return *t;
+  Tuple ret;
+  bool find = false;
+
+  do{
+    serviceQueue.p();
+    readCountAccess.p();
+    if(readCount.equalZero())
+      resourceAccess.p();
+    readCount.v();
+    serviceQueue.v();
+    readCountAccess.v();
+
+    t = Find_(describe);
+
+    if(t != nullptr){
+      ret = *t;
+      find = true;
+    }
+
+    readCountAccess.p();
+    readCount.p();
+    if(readCount.equalZero())
+      resourceAccess.v();
+    readCountAccess.v();
+
+    if(find)
+      return ret;
+  } while (sleep(1));  // or other option for waiting/sleeping
 }
 
 Tuple *Linda::Find_(const TupleDesc &td) {
