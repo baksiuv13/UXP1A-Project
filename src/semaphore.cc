51983c4148472cc1afebe4086f356233639d4bd9
Kamil Mankowski (kam193) <kam193@wp.pl>
2019-06-09 10:31:56 +0200

    Simplify return statement, put semaphores inside single namespace block

--- src/semaphore.cc
+++ src/semaphore.cc
@@ -2,32 +2,25 @@
 
 #include "src/semaphore.h"
 
-const char uxp::Semaphore::SEM_KEY_PATH[] = "./semkeypath";
-const int32_t uxp::Semaphore::SEM_KEY_PROJ_ID = 13;
-const int32_t uxp::Semaphore::N_SEMS = 13;
-const key_t uxp::Semaphore::SEM_KEY = ftok(SEM_KEY_PATH, SEM_KEY_PROJ_ID);
-const int32_t uxp::Semaphore::SEM_ID =
-    semget(SEM_KEY, N_SEMS, 0666 | IPC_CREAT);
+namespace uxp {
 
-bool uxp::Semaphore::initializeAll(const int32_t value) {
+const char Semaphore::SEM_KEY_PATH[] = "./semkeypath";
+const int32_t Semaphore::SEM_KEY_PROJ_ID = 13;
+const int32_t Semaphore::N_SEMS = 13;
+const key_t Semaphore::SEM_KEY = ftok(SEM_KEY_PATH, SEM_KEY_PROJ_ID);
+const int32_t Semaphore::SEM_ID = semget(SEM_KEY, N_SEMS, 0666 | IPC_CREAT);
+
+bool Semaphore::initializeAll(const int32_t value) {
   int rc = semctl(SEM_ID, 0, SETALL, value);
-  if (rc < 0) {
-    return false;
-  } else {
-    return true;
-  }
+  return !(rc < 0);
 }
 
-bool uxp::Semaphore::closeSemTable() {
+bool Semaphore::closeSemTable() {
   int rc = semctl(SEM_ID, 0, IPC_RMID);  // 0 is ignored
-  if (rc < 0) {
-    return false;
-  } else {
-    return true;
-  }
+  return !(rc < 0);
 }
 
-bool uxp::Semaphore::P() {
+bool Semaphore::P() {
   if (semNum >= 0 && semNum < N_SEMS) {
     struct sembuf operation = {semNum, -1, SEM_UNDO};
     semop(SEM_ID, &operation, 1);
@@ -37,7 +30,7 @@ bool uxp::Semaphore::P() {
   }
 }
 
-bool uxp::Semaphore::V() {
+bool Semaphore::V() {
   if (semNum >= 0 && semNum < N_SEMS) {
     struct sembuf operation = {semNum, 1, SEM_UNDO};  //  1 - operation value
     semop(SEM_ID, &operation, 1);                     // 1 - only one operation
@@ -47,15 +40,13 @@ bool uxp::Semaphore::V() {
   }
 }
 
-bool uxp::Semaphore::initialize(const int32_t value) {
+bool Semaphore::initialize(const int32_t value) {
   int rc = semctl(SEM_ID, semNum, SETVAL, value);
-  if (rc < 0) {
-    return false;
-  } else {
-    return true;
-  }
+  return !(rc < 0);
 }
 
-int32_t uxp::Semaphore::getValue() { return semctl(SEM_ID, semNum, GETVAL); }
+int32_t Semaphore::getValue() { return semctl(SEM_ID, semNum, GETVAL); }
 
-bool uxp::Semaphore::isZero() { return !getValue(); }
+bool Semaphore::isZero() { return !getValue(); }
+
+}  // namespace uxp
