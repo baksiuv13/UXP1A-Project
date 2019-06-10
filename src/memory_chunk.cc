da42f4cd626d9c955c4472deabb3f962a6dcd888
LubiePierogi <26669430+LubiePierogi@users.noreply.github.com>
2019-06-10 16:18:11 +0200

    removed throwing exceptions from destructor

--- src/memory_chunk.cc
+++ src/memory_chunk.cc
@@ -18,6 +18,11 @@ namespace {
 static constexpr int create_mem_flag = IPC_CREAT | IPC_EXCL | 0660;
 static constexpr int open_existing_mem_flag = 0660;
 
+static void ERROR_PRINT(const char *msg) {
+  std::cerr << std::string(msg) + ":\n\t" + std::strerror(errno) + '\n';
+  errno = 0;
+}
+
 static std::string EXCEPTION_MSG(const char *msg) {
   int e = errno;
   errno = 0;
@@ -73,18 +78,24 @@ void MemoryChunk::AttachMem_(const char *path, size_t size) {
 
 
 void MemoryChunk::CloseMem_() {
-  int res = shmdt(address_);
-  if (res < 0) {
-    Throw<std::runtime_error>("Could not detach memory block");
+  int dt_res = shmdt(address_);
+  if (dt_res < 0) {
+    ERROR_PRINT("Could not detach memory block");
   }
   struct shmid_ds shmds;
-  res = shmctl(shm_id_, IPC_STAT, &shmds);
-  if (res < 0) {
-    Throw<std::runtime_error>("Could not read info about memory block");
+  int stat_res = shmctl(shm_id_, IPC_STAT, &shmds);
+  if (stat_res < 0) {
+    ERROR_PRINT("Could not read info about memory block");
   }
+  int rem_res = 0;
   bool remove = shmds.shm_nattch == 0;
   if (remove) {
-    shmctl(shm_id_, IPC_RMID, nullptr);
+    rem_res = shmctl(shm_id_, IPC_RMID, nullptr);
+    if (rem_res < 0)
+      ERROR_PRINT("Could not remove memory block");
+  }
+  if (dt_res < 0 || stat_res < 0 || rem_res < 0) {
+    std::terminate();
   }
 }
 
